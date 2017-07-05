7421b7df370503cab00ef1583e6c26343b56b9ba
Void Bird <1918601366@qq.com>
2017-07-05 17:11:27 +0800

    use class-based view

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,11 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^latest\.html$', views.IndexView.as_view(), name='latest'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultView.as_view(), name='result'),
+    url(r'^(?P<pk>[0-9]+)/vote/$', views.vote, name='vote')
 ]
